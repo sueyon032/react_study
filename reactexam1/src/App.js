bdcf359f6a911a1ff9d7ee4ab46d2369d3e73b49
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-12-16 01:07:58 +0900

    React.js 기초 | JSX

--- reactexam1/src/App.js
+++ reactexam1/src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
+import React from 'react';
+// import './App.css';
+
+import MyHeader from './MyHeader';
 
 function App() {
-  let name="김수연";
+
+  const style= {
+    App:{
+      backgroundColor: "black", 
+    },
+    h2: {
+      color: "pink",
+    },
+    bold_text: {
+      color: "skyblue",
+    }
+  }
+
+  const number=5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트 {name}</h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+      <h2 style={style.h2}>안녕 리액트</h2>
+      <b style={style.bold_text} id='bold_text'>
+        {number}는: {number%2===0 ? "짝수" : "홀수"}
+      </b>
     </div>
   );
 }
---
+++ reactexam1/src/MyFooter.js
@@ -0,0 +1,3 @@
+const MyFooter=()=>{};
+
+export default MyFooter;
