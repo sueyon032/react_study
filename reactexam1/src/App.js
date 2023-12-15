184f10963b03dae366ea7e8f906770d650d928ef
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-12-15 20:07:51 +0900

    React.js 기초 | Create React App

---
+++ reactexam1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  let name="김수연";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트 {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
