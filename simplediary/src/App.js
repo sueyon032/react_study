0b2886a468125995b0f638b5fab1df1fd7b66e99
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-12-21 22:12:42 +0900

    React 기본 - 간단한 일기장 프로젝트 | React에서 배열 사용하기 1 - 리스트 렌더링 (조회)

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,12 +1,39 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+import "./App.css";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "김수연",
+    content: "하이 1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "나수연",
+    content: "하이 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "다수연",
+    content: "하이 3",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <DiaryEditor/>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
-}
+};
 
 export default App;
