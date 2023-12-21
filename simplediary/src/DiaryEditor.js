570bf287c2e18edf1a06bc057c99a8d21b69314b
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-12-21 20:21:20 +0900

    React 기본 - 간단한 일기장 프로젝트 | React에서 DOM  - useRef

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+    
+    const authorInput= useRef();
+    const contentInput= useRef();
+
     const [state, setState] = useState({
         author: "",
         content: "",
@@ -15,7 +19,19 @@ const DiaryEditor = () => {
     };
 
     const handleSubmit = () => {
-        console.log(state);
+        if (state.author.length < 1) {
+            // alert("작성자는 최소 1글자 이상 입력해주세요");
+            // focus
+            authorInput.current.focus();
+            return;
+        }
+
+        if (state.content.length < 5) {
+            // alert("일기 본분은 최소 5글자 이상 입력해주세요");
+            // focus
+            contentInput.current.focus();
+            return;
+        }
         alert("저장 성공");
     };
 
@@ -24,6 +40,7 @@ const DiaryEditor = () => {
             <h2>오늘의 일기</h2>
             <div>
                 <input
+                    ref={authorInput}
                     name="author"
                     value={state.author}
                     onChange={handleChangeState}
@@ -31,6 +48,7 @@ const DiaryEditor = () => {
             </div>
             <div>
                 <textarea
+                    ref={contentInput}
                     name="content"
                     value={state.content}
                     onChange={handleChangeState} />
