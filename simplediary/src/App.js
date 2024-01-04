21b45092c84bb1f305ccb6372d0e395ad5904be1
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-01-04 17:24:01 +0900

    React 기본 - 간단한 일기장 프로젝트 | React에서 API 호출하기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -2,31 +2,8 @@ import React, { useState, useEffect, useRef } from "react";
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 import "./App.css";
-import LifeCycle from "./LifeCycle";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "김수연",
-//     content: "하이 1",
-//     emotion: 1,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "나수연",
-//     content: "하이 2",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "다수연",
-//     content: "하이 3",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-// ]
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 
@@ -34,6 +11,25 @@ function App() {
 
   const dataId= useRef(0);
 
+  const getData= async()=>{
+    const res= await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=> res.json());
+    
+    const initData= res.slice(0,20).map((it)=>{
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random()*5)+1,
+        create_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    });
+    setData(initData);
+  };
+
+  useEffect(()=>{
+    getData();
+  }, []);
+  
   const onCreate= (author, content, emotion)=> {
     const created_date= new Date().getTime();
     const newItem= {
@@ -63,7 +59,6 @@ function App() {
 
   return (
     <div className="App">
-      <LifeCycle/>
       <DiaryEditor onCreate={onCreate}/>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
     </div>
