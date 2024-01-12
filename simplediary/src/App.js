64580303a04f47fc6f2dcda9bb1cb901cb7bf078
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-01-12 16:05:47 +0900

    React 기본 - 간단한 일기장 프로젝트 | 최적화 1 - useMemo

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 import "./App.css";
@@ -7,18 +7,18 @@ import "./App.css";
 
 function App() {
 
-  const [data, setData]= useState([]);
+  const [data, setData] = useState([]);
 
-  const dataId= useRef(0);
+  const dataId = useRef(0);
 
-  const getData= async()=>{
-    const res= await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=> res.json());
-    
-    const initData= res.slice(0,20).map((it)=>{
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
       return {
         author: it.email,
         content: it.body,
-        emotion: Math.floor(Math.random()*5)+1,
+        emotion: Math.floor(Math.random() * 5) + 1,
         create_date: new Date().getTime(),
         id: dataId.current++
       }
@@ -26,41 +26,60 @@ function App() {
     setData(initData);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
   }, []);
-  
-  const onCreate= (author, content, emotion)=> {
-    const created_date= new Date().getTime();
-    const newItem= {
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
       author,
-      content, 
+      content,
       emotion,
       created_date,
       id: dataId.current,
     };
-    dataId.current+=1;
+    dataId.current += 1;
     setData([newItem, ...data]);
   };
 
-  const onRemove= (targetId)=> {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다.`);
-    const newDiaryList= data.filter((it)=> it.id!==targetId);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
 
-  const onEdit =(targetId, newContent)=> {
+  const onEdit = (targetId, newContent) => {
     setData(
-      data.map((it)=>
-        it.id=== targetId? {...it, content : newContent} : it
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
       )
     )
   }
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return {
+      goodCount,
+      badCount,
+      goodRatio
+    };
+  }, [data.length]);
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+
   return (
     <div className="App">
-      <DiaryEditor onCreate={onCreate}/>
-      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
+      <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio }</div>
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 };
