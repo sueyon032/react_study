b9ffd3e328159e2651ccd3bf95d198929e03050b
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-01-25 20:18:40 +0900

    React 실전 프로젝트 - 감정 일기장 만들기 | 페이지 구현 - 일기 쓰기 (/new)

--- emotion-diary/src/pages/New.js
+++ emotion-diary/src/pages/New.js
@@ -1,10 +1,11 @@
-const New = ()=> {
+import DiaryEditor from "../components/DiaryEditor";
+
+const New = () => {
     return (
         <div>
-            <h1>New</h1>
-            <p>이곳은 일기 작성페이지 입니다.</p>
+            <DiaryEditor />
         </div>
     );
-}
+};
 
 export default New;
