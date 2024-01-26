0bab15b152085e1e951dd51552f46fc3ec04acd9
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-01-26 22:32:09 +0900

    React 실전 프로젝트 - 감정 일기장 만들기 | 페이지 구현 - 일기 쓰기 (/new)

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,27 +1,32 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
-const Edit = ()=> {
+const Edit = () => {
 
-    const navigate= useNavigate();
-    const [searchParms, setSearchParms] = useSearchParams();
+    const [originData, setOriginData] = useState();
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const diaryList = useContext(DiaryStateContext);
 
-    const id= searchParms.get("id");
-    console.log("id: ", id);
+    useEffect(() => {
+        if (diaryList.length >= 1) {
+            const targetDiary = diaryList.find(
+                (it) => parseInt(it.id) === parseInt(id)
+            );
 
-    const mode= searchParms.get("mode");
-    console.log("mode: ", mode);
+            if (targetDiary) {
+                setOriginData(targetDiary);
+            } else {
+                navigate("/", { replace: true });
+            }
+        }
+    }, [id, diaryList])
 
     return (
         <div>
-            <h1>Edit</h1>
-            <p>이곳은 일기 수정 페이지 입니다.</p>
-            <button onClick={()=> setSearchParms({who: "suyeon"})}>QS바꾸기</button>
-            <button onClick={()=> {
-                navigate("/home");
-            }}>HOME으로 가기</button>
-            <button onClick={()=> {
-                navigate(-1);
-            }}>뒤로가기</button>
+            {originData && <DiaryEditor isEdit={true} originData={originData} />}
         </div>
     );
 }
