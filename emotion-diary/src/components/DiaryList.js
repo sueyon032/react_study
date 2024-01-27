17a2183f978960ce27d3b3ae11688e95f3794ce5
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-01-27 17:29:49 +0900

    React 실전 프로젝트 - 감정 일기장 만들기 | (서브 챕터) 흔히 발생하는 버그 수정 하기

--- emotion-diary/src/components/DiaryList.js
+++ emotion-diary/src/components/DiaryList.js
@@ -28,7 +28,7 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
     const navigate = useNavigate();
-    const [sortType, setSortType] = useState("lastest");
+    const [sortType, setSortType] = useState("latest");
     const [filter, setFilter] = useState("all");
 
     const getProssedDiaryList = () => {
@@ -41,7 +41,7 @@ const DiaryList = ({ diaryList }) => {
         };
 
         const compare = (a, b) => {
-            if (sortType === "lastest") {
+            if (sortType === "latest") {
                 return parseInt(b.date) - parseInt(a.date);
             } else {
                 return parseInt(a.date) - parseInt(b.date);
